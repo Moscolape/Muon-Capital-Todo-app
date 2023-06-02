4818b4e55ec3f9b94ecd0c95ca2d65d67f8c6fd3
Moscolape <chunkums088@gmail.com>
2023-06-02 17:45:02 +0100

    add static todo lists

--- src/components/todos/todos.component.js
+++ src/components/todos/todos.component.js
@@ -4,6 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
+import { ReactComponent as Oval } from "../../assets/Oval 2.svg";
+import { ReactComponent as Edit } from "../../assets/Group 16106.svg";
+
+
 
 const Todos = () => {
     return (
@@ -12,17 +16,33 @@ const Todos = () => {
                 <div className="todo-component">
                     <div className="todo-pack">
                         <div className="todo-list">List: Things to Buy</div>
-                        <p>lorem</p>
-                        <p>lorem</p>
-                        <p>lorem</p>
-                        <p>loremwererwerwerwrerewrewrwerewrw</p>
+                        <div className="describe-todo">
+                            <div className="add-todo">
+                                <span><Oval/></span>
+                                <input placeholder="Add Todo"/>
+                            </div>
+                            <input className="describe" placeholder="Add Todo Description"/>
+                            <span className="plus"><FontAwesomeIcon icon={faPlus} /></span>
+                        </div>
+                        <div className="describe-todo">
+                            <div className="add-todo">
+                                <span><Oval/></span>
+                                <span className="todo-one">Carrot</span>
+                            </div>
+                            <input className="describe" placeholder="Add Todo Description"/>
+                            <span className="plus"><Edit/></span>
+                        </div>
                     </div>
                     <div className="todo-pack">
                         <div className="todo-list">List: Empty List</div>
-                        <p>lorem</p>
-                        <p>lorem</p>
-                        <p>lorem</p>
-                        <p>loremwererwerwerwrerewrewrwerewrw</p>
+                        <div className="describe-todo">
+                            <div className="add-todo">
+                                <span><Oval/></span>
+                                <input placeholder="Add Todo"/>
+                            </div>
+                            <input className="describe" placeholder="Add Todo Description"/>
+                            <span className="plus"><FontAwesomeIcon icon={faPlus} /></span>
+                        </div>
                     </div>
                     <div style={{position: 'relative'}}>
                         <input type="text" className="todo-list" placeholder="Add Todo-List"/>
